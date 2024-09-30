fef8f67dade3ce596104b3a7aab774a20a28d8b8
Jou0001 <jout0001@edu.zealand.dk>
2024-09-30 13:55:52 +0200

    Add project files.

---
+++ TCP1/TCPClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace TCP1
+
+{
+
+
+    public class TcpClientProgram
+    {
+        public void Start()
+        {
+            Console.WriteLine("TCP Client started");
+
+            try
+            {
+                using TcpClient client = new TcpClient("127.0.0.1", 7);
+                NetworkStream ns = client.GetStream();
+                StreamReader reader = new StreamReader(ns);
+                StreamWriter writer = new StreamWriter(ns);
+
+                // Spørger brugeren om kommandoen (Random, Add, Subtract)
+                Console.WriteLine("Enter command (Random, Add, Subtract): ");
+                string command = Console.ReadLine();
+                writer.WriteLine(command);
+                writer.Flush();
+
+                // Venter på serverens svar ("Input numbers")
+                string serverResponse = reader.ReadLine();
+                Console.WriteLine("Server says: " + serverResponse);
+
+                // Spørger brugeren om tal1 og tal2
+                Console.WriteLine("Enter two numbers separated by a space: ");
+                string numbers = Console.ReadLine();
+                writer.WriteLine(numbers);
+                writer.Flush();
+
+                // Modtager resultatet fra serveren
+                string result = reader.ReadLine();
+                Console.WriteLine("Server result: " + result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ TCP1/TCPServer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TCP1
+{
+    public class TcpServer
+    {
+        public static void Main()
+        {
+            Console.WriteLine("TCP Server started");
+            TcpListener listener = new TcpListener(IPAddress.Any, 7);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                IPEndPoint clientEndPoint = socket.Client.RemoteEndPoint as IPEndPoint;
+                Console.WriteLine("Client connected: " + clientEndPoint.Address);
+
+                // Håndter hver klient i en ny tråd
+                Task.Run(() => HandleClient(socket));
+            }
+        }
+
+        static void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            Random random = new Random();
+
+            try
+            {
+                while (socket.Connected)
+                {
+                    // Læs kommando fra klienten (Random, Add, Subtract)
+                    string? command = reader.ReadLine();
+                    Console.WriteLine("Received command: " + command);
+
+                    if (command == null) break;
+
+                    // Svar tilbage til klienten
+                    writer.WriteLine("Input numbers");
+                    writer.Flush();
+
+                    // Læs tal1 og tal2 fra klienten
+                    string? numberInput = reader.ReadLine();
+                    if (numberInput == null) break;
+
+                    string[] numbers = numberInput.Split(' ');
+                    if (numbers.Length != 2 ||
+                        !int.TryParse(numbers[0], out int num1) ||
+                        !int.TryParse(numbers[1], out int num2))
+                    {
+                        writer.WriteLine("Invalid input");
+                        writer.Flush();
+                        continue;
+                    }
+
+                    // Håndtering af kommandoer
+                    if (command == "Random")
+                    {
+                        int randomNumber = random.Next(num1, num2 + 1);
+                        writer.WriteLine(randomNumber);
+                    }
+                    else if (command == "Add")
+                    {
+                        int sum = num1 + num2;
+                        writer.WriteLine(sum);
+                    }
+                    else if (command == "Subtract")
+                    {
+                        int result = num1 - num2;
+                        writer.WriteLine(result);
+                    }
+                    else
+                    {
+                        writer.WriteLine("Unknown command");
+                    }
+
+                    writer.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                socket.Close();
+            }
+        }
+    }
+}
+
